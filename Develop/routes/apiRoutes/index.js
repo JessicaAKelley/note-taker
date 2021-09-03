5f7fd52e40bb4f26a02350d68a75a42e19f21637
JessicaAKelley <kelleyajessica@gmail.com>
2021-09-03 14:06:06 -0500

    Adding and deleting note and unique ID for notes

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,7 +1,21 @@
 const router = require("express").Router();
-const [notes] = require("../../db/db.json");
+const notes = require("../../db/db.json");
+var uniqid = require("uniqid");
 
 router.get("/notes", (req, res) => {
+    res.json(notes);
+});
+
+router.post("/notes", (req, res) => {
+    req.body.id = uniqid();
+    notes.push(req.body);
+    res.json(notes);
+});
+
+router.delete("/notes/:id", (req, res) => {
+    const { id } = req.params;
+    const projectIndex = notes.findIndex((p) => p.id == id);
+    notes.splice(projectIndex, 1);
     res.json(notes);
 });
 
