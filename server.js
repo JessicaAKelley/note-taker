48234a03b00ec7ae4afb64ad5889e1dc500f579e
JessicaAKelley <kelleyajessica@gmail.com>
2021-09-03 12:23:39 -0500

    API routes setup

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -6,9 +7,12 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// const htmlRoutes = require("./routes/htmlRoutes");
+const htmlRoutes = require("./Develop/routes/htmlRoutes");
+const apiRoutes = require("./Develop/routes/apiRoutes");
 
-// app.use("/", htmlRoutes);
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
 
 app.listen(PORT, () => {
     console.log("API server now listening on port 3001");
