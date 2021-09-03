daae58c20d837a429117537b19c6d79ac43943ab
JessicaAKelley <kelleyajessica@gmail.com>
2021-09-03 14:23:38 -0500

    Fixed static path, now working

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const path = require("path");
-const express = require("express");
-
 const PORT = process.env.PORT || 3001;
+
+const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("./Develop/public"));
 
 const htmlRoutes = require("./Develop/routes/htmlRoutes");
 const apiRoutes = require("./Develop/routes/apiRoutes");
